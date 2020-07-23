aa22b92c0d426e9227462b162f5dba8cfab65357
superflouz <superflouz@hotmail.com>
2020-07-23 16:02:36 +0200

    Add interaction to player controller

--- Assets/Scripts/Globals/Globals.cs
+++ Assets/Scripts/Globals/Globals.cs
@@ -6,11 +6,11 @@ public static class Globals
 { 
     public static Vector2 RadianToVector2(float radian)
     {
-        return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+        return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian)).normalized;
     }
 
     public static Vector2 DegreeToVector2(float degree)
     {
-        return RadianToVector2(degree * Mathf.Deg2Rad);
+        return RadianToVector2(degree * Mathf.Deg2Rad).normalized;
     }
 }
